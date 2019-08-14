e2780fffe4989578eec6be2afa17039c8b96cd33
jenny <jenn7nm@gmail.com>
2019-08-14 00:49:21 -0400

    add login form class

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -56,6 +56,7 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError('That email is taken. Please choose a different one.')
 
 
+                
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
